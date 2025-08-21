2c566316bd046b12b091743938c2798b374fbdae
Parwej Alam <parwejalamgt@gmail.com>
2025-08-21 18:08:02 +0530

    feat: implement swiper for project carousel and update styles

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,19 +1,55 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, ViewChild, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { CommonModule } from '@angular/common';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { ThemeService } from '../services/theme.service';
+import Swiper from 'swiper';
+
+
 declare var ScrollReveal: any;
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
   standalone: true,
-  imports: [CommonModule, MdbCarouselModule]
+  imports: [CommonModule, MdbCarouselModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ProjectComponent {
 
+  constructor(private ThemeService: ThemeService) { }
+  themeColor = (this.ThemeService.getTheme()) === 'dark' ? false : true;
+
   projects = [
     {
-      src: 'assets/projects/AMS.png',
+      src: 'assets/projects/Qaida.png',
+      link: ''
+    },
+    {
+      src: 'assets/projects/Qaida2.png',
+      link: ''
+    },
+    {
+      src: 'assets/projects/portfolio.png',
+      link: ''
+    },
+    {
+      src: 'assets/projects/MEAN_Project_Dark.png',
+      link: ''
+    },
+    {
+      src: 'assets/projects/MEAN_Project_Light.png',
+      link: ''
+    },
+    {
+      src: 'assets/projects/Noble-wave-openData.png',
+      link: ''
+    },
+    {
+      src: 'assets/projects/sevelt-task-tracker.png',
+      link: ''
+    },
+    {
+      src: 'assets/projects/snake-game.png',
       link: ''
     },
     {
@@ -24,6 +60,5 @@ export class ProjectComponent {
       src: 'assets/projects/AMS.png',
       link: ''
     },
-
   ]
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
